9dff550d6a5347dbca503a13ad0004bce1a360a3
Soham <deadly.soham@gmail.com>
2021-09-20 13:30:02 +0530

    4 : increased lines of guessing game from 17 to 20

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 # The Unix Workbench
-
-<div align=center>## Peer Gradded Assignment </div>
-
-### Guessing Game
-
-* Makefile was executed on 18/09/21 at 23:39
-
-* The source code had 17 lines
+\n<div align=center>## Peer Gradded Assignment </div>
+\n### Guessing Game
+\n* Makefile was executed on  at 
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,9 @@
 let value=$(echo | ls -l | wc -l)-1 # counting total files -1 because 1st line of ls -l command is not a file
 echo "Guess :" && read n
+
 function check # a function
-{ # If statements
+{ 
+	# If statements
 	if [[ n*2 -le value ]] # when guess is less than half the number of files
 	then
 		echo "Too Low" # Low Message
@@ -10,6 +12,7 @@ function check # a function
 		echo "Too high" # High Message
 	fi
 }
+
 while [[ n -ne value ]] # this will terminate only when guess correctly
 do
 	check && echo "Try Again :" && read n  # Retry
